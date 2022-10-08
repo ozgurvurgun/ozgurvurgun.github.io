d2690c89c71ebb70efab896636d1828781489684
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2022-10-08 14:18:42 +0300

    creadted

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <link rel="icon" href="image/icon.png" type="image/x-icon">
+    <meta name="description" content="Özgür Vurgun' un kişisel web sitesi." />
+    <link rel="stylesheet" type="text/css" href="meStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="OjEh7WK4lQjpnG7YCYNG3k6TyDBsmunvKRvfOLHDVQY" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="google" content="sitelinkssearchbox" />
+    <meta name="googlebot" content="index, follow" />
+    <meta name="robots" content="index,follow">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/eb7367d676.js" crossorigin="anonymous"></script>
+    <title>Özgür Vurgun</title>
+</head>
+
+<body style="font-family:  'Raleway', sans-serif;padding-top:55px">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" id="OzgurSize" href="panelgiris.php">Özgür V.</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul class="navbar-nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="aboutMe.php">Hakkımda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="photos.php">Galeri</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Projelerim
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="projects.php">Punch Engine (Yumruk Motoru)</a></li>
+                            <li><a class="dropdown-item disabled" href="#">Kuantum Sensörler</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="blog.php">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">İletişim</a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link" href="IP.php">IP Adresin</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="image/yazilim.jpg" class="d-block w-100 photosize" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/web.jpg" class="d-block w-100 photosize" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/cpu.jpeg" class="d-block w-100 photosize" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <section class="p-5 text-center homepage">
+        <div class="container mt-5">
+            <hr class="hrsetting"><i class="fa-solid fa-user-astronaut iconsetting"></i>
+
+            <div class="container text-center">
+
+                <div class="row">
+
+                    <div class="col-md-4 mx-auto">
+                        <div class="card m-auto mb-5 mt-4 shadow">
+                            <img src="image/hadron2.jpeg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Büyük Hadron Çarpıştırıcısı</h5>
+                                <p class="card-text">
+                                    Büyük Hadron Çarpıştırıcısı (LHC, İngilizce Large Hadron Collider) dünyanın en büyük
+                                    ve en güçlü parçacık çarpıştırıcısı ve dünyanın en büyük makinesi. </p>
+                                <a href="https://tr.wikipedia.org/wiki/B%C3%BCy%C3%BCk_Hadron_%C3%87arp%C4%B1%C5%9Ft%C4%B1r%C4%B1c%C4%B1s%C4%B1"
+                                    target="_blank" class="btn btn-primary">Devamını Oku</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mx-auto">
+                        <div class="card m-auto mb-5 mt-4 shadow">
+                            <img src="image/ciftyarik5.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Çift Yarık Deneyi</h5>
+                                <p class="card-text">Young deneyi olarak da bilinen çift-yarık deneyi, ışığın dalga
+                                    özelliği sergilediğini gösterir.
+                                    Fotoelektrik etkisi ışığın dalga özelliğinin yanı sıra parçacık özelliği de
+                                    sergilediğini gösterir.</p>
+                                <a href="https://tr.wikipedia.org/wiki/%C3%87ift_yar%C4%B1k_deneyi" target="_blank"
+                                    class="btn btn-primary">Devamını Oku</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mx-auto">
+                        <div class="card m-auto mb-5 mt-4 shadow">
+                            <img src="image/katlakes3.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Katla ve Kes Teoremi</h5>
+                                <p class="card-text">The fold-and-cut theorem states that any shape with straight sides
+                                    can be cut from a single
+                                    (idealized) sheet of paper by folding it flat and making a single straight complete
+                                    cut.</p>
+                                <a href="http://erikdemaine.org/foldcut/" target="_blank"
+                                    class="btn btn-primary">Devamını Oku</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mx-auto">
+                        <div class="card m-auto mb-5 shadow mt-4">
+                            <img src="image/termobla.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Termodinamiğin 1. Yasası</h5>
+                                <p class="card-text">The first law of thermodynamics is a version of the law of
+                                    conservation of energy, adapted for thermodynamic
+                                    processes, distinguishing three kinds of transfer of energy, as heat, as
+                                    thermodynamic work,
+                                    and as energy associated with matter transfer, and relating them to a function of a
+                                    body's state, called internal energy.
+                                </p>
+                                <a href="https://en.wikipedia.org/wiki/First_law_of_thermodynamics" target="_blank"
+                                    class="btn btn-primary">Devamını Oku</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mx-auto">
+                        <div class="card m-auto mb-5 mt-4 shadow">
+                            <img src="image/emreyilmaz.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">delirehberi</h5>
+                                <p class="card-text">Bu kanalda Symfony konusundaki videolar başta olmak üzere zaman
+                                    zaman PHP, Javascript, NodeJS ve
+                                    diğer yazilim dilleri ile ilgili videolar bulabilirsiniz.
+                                    Videoların arasında uzun süreler bulunsa da kanalda video paylaşılmaya devam edecek.
+                                </p>
+                                <br>
+                                <a href="https://www.youtube.com/c/EmreYILMAZ" target="_blank"
+                                    class="btn btn-primary">&nbsp;&nbsp;Kanala Git&nbsp;&nbsp;</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4 mx-auto">
+                        <div class="card m-auto mb-5 mt-4 shadow">
+                            <img src="image/bilimkutusu.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Bilim Kutusu</h5>
+                                <p class="card-text">Bilim Kutusu, yenilikçi yaklaşımla çocukların sorarak ve
+                                    deneyimleyerek bilimsel konuları
+                                    kavramasını ve sevmesini sağlayan deney setleri içeren macera kutusudur.
+                                    Çocuklarınızın eğitici ve
+                                    kaliteli zaman geçirmesini, bilimsel alt yapısını oluşturup ufkunu genişletmesini
+                                    istiyorsanız bir
+                                    bakın derim.</p>
+                                <br>
+                                <a href="https://bilimkutusu.com/tr_TR/" target="_blank"
+                                    class="btn btn-primary">&nbsp;&nbsp;Sayfaya Git&nbsp;&nbsp;</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+    </section>
+
+    <div class="text-center">
+        <p class="align-middle p-4">
+            <span>all rights reserved &copy; | 2022</span>
+            <br>
+            <span>Designer and Developer <b>ÖZGÜR VURGUN</b></span>
+        </p>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ photos.php
@@ -0,0 +1,184 @@
+
+<!DOCTYPE html>
+<html lang="tr">
+
+<head>
+    <link rel="icon" href="image/icon.png" type="image/x-icon">
+    <meta name="description" content="Galeri" />
+    <link rel="stylesheet" type="text/css" href="meStyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="OjEh7WK4lQjpnG7YCYNG3k6TyDBsmunvKRvfOLHDVQY" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="google" content="sitelinkssearchbox" />
+    <meta name="googlebot" content="index, follow" />
+    <meta name="robots" content="index,follow">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/eb7367d676.js" crossorigin="anonymous"></script>
+
+    <title>Galeri | Özgür Vurgun</title>
+</head>
+
+<body style="font-family:  'Raleway', sans-serif;padding-top:55px">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" id="OzgurSize" href="panelgiris.php">Özgür V.</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul class="navbar-nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="aboutMe.php">Hakkımda</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="photos.php">Galeri</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Projelerim
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="projects.php">Punch Engine (Yumruk Motoru)</a></li>
+                            <li><a class="dropdown-item disabled" href="#">Kuantum Sensörler</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="blog.php">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">İletişim</a>
+                    </li>
+                    <li class="nav-item me-2">
+                        <a class="nav-link" href="IP.php">IP Adresin</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section class="p-5 text-center homepage">
+        <div class="container mt-5">
+            <hr class="hrsetting"><i class="fa-solid fa-camera-retro iconsetting"></i>
+
+            <div class="row">
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/bir.jpeg" target="_blank">
+                            <img src="image/bir.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/iki.jpeg" target="_blank">
+                            <img src="image/iki.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/uc.jpeg" target="_blank">
+                            <img src="image/uc.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/dort.jpeg" target="_blank">
+                            <img src="image/dort.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/bes.jpeg" target="_blank">
+                            <img src="image/bes.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/alti.jpeg" target="_blank">
+                            <img src="image/alti.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/yedi.jpeg" target="_blank">
+                            <img src="image/yedi.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/sekiz.jpeg" target="_blank">
+                            <img src="image/sekiz.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/dokuz.jpeg" target="_blank">
+                            <img src="image/dokuz.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/on.jpeg" target="_blank">
+                            <img src="image/on.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/onbir.jpeg" target="_blank">
+                            <img src="image/onbir.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+
+                <div class="col-md-3">
+                    <figure class="figure">
+                        <a href="image/oniki.jpeg" target="_blank">
+                            <img src="image/oniki.jpeg" class="figure-img img-fluid rounded" alt="...">
+                        </a>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="text-center">
+        <p class="align-middle p-4">
+            <span>all rights reserved &copy; | 2022</span>
+            <br>
+            <span>Designer and Developer <b>ÖZGÜR VURGUN</b></span>
+        </p>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
+</html>
