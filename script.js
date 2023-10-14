f8bf4ec6821f9860d22e8abaa371e2ddcdad94ce
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-14 22:41:20 +0300

    pwa example added

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-navigator.serviceWorker.register('sw.js');
-Notification.requestPermission(function(result) {
-  if (result === 'granted') {
-    navigator.serviceWorker.ready.then(function(registration) {
-      registration.showNotification('Notification with ServiceWorker');
+let button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    navigator.serviceWorker.register('sw.js');
+    Notification.requestPermission(function(result) {
+      if (result === 'granted') {
+        navigator.serviceWorker.ready.then(function(registration) {
+          registration.showNotification('Notification with ServiceWorker');
+        });
+      }
     });
-  }
 });
+
