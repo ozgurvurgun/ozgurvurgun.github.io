28ee72a53eecde0515d0b3c56e89c5d7976807fd
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-14 13:40:25 +0300

    pwa example edited

--- pwa-example/serviceWorker.js
+++ pwa-example/serviceWorker.js
@@ -1,16 +1,20 @@
-const staticDevCoffee = "dev-coffee-site-v1"
+const staticDevCoffee = "dev-coffee-site-v1";
 const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/images/1667593744391.jpg"
-]
+  "https://ozgurvurgun.github.io/pwa-example/",
+  "https://ozgurvurgun.github.io/pwa-example/index.html",
+  "https://ozgurvurgun.github.io/pwa-example/css/style.css",
+  "https://ozgurvurgun.github.io/pwa-example/js/app.js",
+  "https://ozgurvurgun.github.io/pwa-example/images/1667593744391.jpg",
+  "https://ozgurvurgun.github.io/pwa-example/images/icons/icon-72x72.png",
+  "https://ozgurvurgun.github.io/pwa-example/images/icons/icon-96x96.png",
+  "https://ozgurvurgun.github.io/pwa-example/images/icons/icon-128x128.png",
+  "https://ozgurvurgun.github.io/pwa-example/images/icons/icon-144x144.png",
+];
 
-self.addEventListener("install", installEvent => {
+self.addEventListener("install", (installEvent) => {
   installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
+    caches.open(staticDevCoffee).then((cache) => {
+      cache.addAll(assets);
     })
-  )
-})
+  );
+});
