b03fde4e8fc7a8535e970b3c5c9015378f6efd8e
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-15 02:17:41 +0300

    pwa example added

--- js/notification.js
+++ js/notification.js
@@ -1,7 +1,9 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    console.log( Notification.requestPermission());
+     Notification.requestPermission().then(()=>{
+        console.log(Notification.permission);
+     });
     navigator.serviceWorker.getRegistrations().then(function(registrations) {
         registrations[0].showNotification("Yeni Mesajınız Var",{
             body: "Ne zamandır yoksun dayı bir uğra ;)",
