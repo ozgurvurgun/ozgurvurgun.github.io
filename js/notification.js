83cc6cae05b91d608e037e63283c976cff67d78a
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-15 02:21:56 +0300

    pwa example added

--- js/notification.js
+++ js/notification.js
@@ -1,13 +1,15 @@
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-     Notification.requestPermission().then(()=>{
-        console.log(Notification.permission);
-     });
-    navigator.serviceWorker.getRegistrations().then(function(registrations) {
-        registrations[0].showNotification("Yeni Mesajınız Var",{
-            body: "Ne zamandır yoksun dayı bir uğra ;)",
-            icon: "icon.png"
+  Notification.requestPermission().then(() => {
+    console.log(Notification.permission);
+    if (Notification.permission === "garanted") {
+      navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        registrations[0].showNotification("Yeni Mesajınız Var", {
+          body: "Ne zamandır yoksun dayı bir uğra ;)",
+          icon: "icon.png",
         });
       });
+    }
+  });
 });
