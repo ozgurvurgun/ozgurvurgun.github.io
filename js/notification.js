73dabe53a757c7828d92511603a60ed14f19b296
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-14 20:34:16 +0300

    pwa example added

--- js/notification.js
+++ js/notification.js
@@ -1,7 +1,9 @@
 // const button = document.querySelector("button");
 window.addEventListener("load", () => {
   Notification.requestPermission().then((perm) => {
+    alert("1. giris")
     if (perm === "granted") {
+        alert("2. giris")
       const notification = new Notification("Ornek bildirim", {
         body: "this is more text",
         icon: "1667593744391.jpg",
