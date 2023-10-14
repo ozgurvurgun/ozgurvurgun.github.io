4f3723fe428fa252c4aa7e4e063fb19a7dfa3dfb
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-14 18:23:57 +0300

    pwa example edited

--- js/notification.js
+++ js/notification.js
@@ -1,6 +1,10 @@
-const button = document.querySelector("button")
-button.addEventListener("click", ()=>{
-    Notification.requestPermission().then(perm =>{
-        alert(perm)
-    })
-})
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  Notification.requestPermission().then((perm) => {
+    if (perm === "granted") {
+      new Notification("Ornek bildirim", {
+        body: "this is more text",
+      });
+    }
+  });
+});
