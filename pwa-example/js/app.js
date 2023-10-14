517a3c803cf8c217f630b0e90db6954880e56347
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-14 12:48:41 +0300

    pwa example added

---
+++ pwa-example/js/app.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container");
+const coffees = [
+  { name: "Perspiciatis", image: "images/1667593744391.jpg" },
+  { name: "Voluptatem", image: "images/1667593744391.jpg" },
+  { name: "Explicabo", image: "images/1667593744391.jpg" },
+  { name: "Rchitecto", image: "images/1667593744391.jpg" },
+  { name: " Beatae", image: "images/1667593744391.jpg" },
+  { name: " Vitae", image: "images/1667593744391.jpg" },
+  { name: "Inventore", image: "images/1667593744391.jpg" },
+  { name: "Veritatis", image: "images/1667593744391.jpg" },
+  { name: "Accusantium", image: "images/1667593744391.jpg" },
+];
+
+const showCoffees = () => {
+  let output = "";
+  coffees.forEach(
+    ({ name, image }) =>
+      (output += `
+                <div class="card">
+                  <img class="card--avatar" src=${image} />
+                  <h1 class="card--title">${name}</h1>
+                  <a class="card--link" href="#">Taste</a>
+                </div>
+                `)
+  );
+  container.innerHTML = output;
+};
+
+document.addEventListener("DOMContentLoaded", showCoffees);
---
+++ pwa-example/serviceWorker.js
@@ -0,0 +1,16 @@
+const staticDevCoffee = "dev-coffee-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/1667593744391.jpg"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
