04a446c7318282cd5eed213c56e6abba8facd447
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-14 19:34:47 +0300

    pwa example added

--- js/notification.js
+++ js/notification.js
@@ -1,39 +1,11 @@
-// const button = document.querySelector("button");
-// button.addEventListener("click", () => {
-//   Notification.requestPermission().then((perm) => {
-//     if (perm === "granted") {
-//       const notification = new Notification("Ornek bildirim", {
-//         body: "this is more text",
-//         data: { hello: "world" },
-//         icon: "1667593744391.jpg"
-//       });
-
-//       notification.addEventListener("close", e =>{
-//         console.log(e);
-//       })
-//     }
-//   });
-// });
-
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
-    Notification.requestPermission().then((result) => {
-      if (result === "granted") {
-        randomNotification();
-      }
-    });
+  Notification.requestPermission().then((perm) => {
+    if (perm === "granted") {
+      const notification = new Notification("Ornek bildirim", {
+        body: "this is more text",
+        icon: "1667593744391.jpg",
+      });
+    }
   });
-
-
-  function randomNotification() {
-    const randomItem = Math.floor(Math.random() * games.length);
-    const notifTitle = games[randomItem].name;
-    const notifBody = `Created by ${games[randomItem].author}.`;
-    const notifImg = `data/img/${games[randomItem].slug}.jpg`;
-    const options = {
-      body: notifBody,
-      icon: notifImg,
-    };
-    new Notification(notifTitle, options);
-    setTimeout(randomNotification, 30000);
-  }
+});
