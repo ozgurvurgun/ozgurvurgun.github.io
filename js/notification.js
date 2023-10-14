2e44f4360e90c8cc9603628e7e8037db0ce0d0e4
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-15 02:05:10 +0300

    pwa example added

--- js/notification.js
+++ js/notification.js
@@ -2,8 +2,8 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
     navigator.serviceWorker.getRegistrations().then(function(registrations) {
-        registrations[0].showNotification("bildirim kafasi",{
-            body: "bu bir deneme mesajıdır",
+        registrations[0].showNotification("Yeni Mesajınız Var",{
+            body: "Ne zamandır yoksun dayı bir uğra ;)",
             icon: "icon.png"
         });
       });
