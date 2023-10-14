7714e7f60f5b4b43b1c1b8686de50f95c9713a00
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-14 22:00:46 +0300

    pwa example added

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const button = document.querySelector("button")
 
 button.addEventListener("click",()=>{
     Notification.requestPermission().then(perm =>{
-        const notification = new Notification("example notification" ,{
-            body: "this is more notification",
-            data: {hello: "world"}
-        })
-
-        notification.addEventListener("error", e =>{
-            alert("error")
-        })
         if (perm =="granted") {
+            const notification = new Notification("example notification" ,{
+                body: "this is more notification",
+                // data: "welcome message",
+            })
 
+            notification.addEventListener("error", e =>{
+                alert("error")
+            })
         }
     })
 })
