9b171839487ad748a139d0a599c5840ff9af0468
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-17 01:47:40 +0300

    pwa example edited

--- js/notification.js
+++ js/notification.js
@@ -7,6 +7,7 @@ const button = document.querySelector("button");
         registrations[0].showNotification("Yeni Mesajınız Var", {
           body: "Ne zamandır yoksun dayı bir uğra ;)",
           icon: "icon.png",
+          showTrigger: new TimestampTrigger(Date.now() + 20 * 1000)
         });
       });
     }
