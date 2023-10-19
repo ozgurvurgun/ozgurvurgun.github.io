c76ffa36be99be77cb9bfe2b05418c43afd9c295
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-20 02:30:20 +0300

    edited

--- js/notification.js
+++ js/notification.js
@@ -1,12 +1,17 @@
 const button = document.querySelector("button");
 
-Notification.requestPermission().then(async () => {
-  if (Notification.permission == "granted") {
-    const registration = await navigator.serviceWorker.getRegistration();
-    registration.showNotification("baslik baslik", {
-      tag: "buda tagmis",
-      body: "This notification was scheduled 30 seconds ago",
-      showTrigger: new TimestampTrigger(Date.now() + 5 * 1000),
-    });
-  }
+
+button.addEventListener("click", ()=>{
+  Notification.requestPermission().then(async () => {
+    if (Notification.permission == "granted") {
+      const registration = await navigator.serviceWorker.getRegistration();
+      registration.showNotification("baslik baslik", {
+        tag: "buda tagmis",
+        body: "This notification was scheduled 30 seconds ago",
+        showTrigger: new TimestampTrigger(Date.now() + 5 * 1000),
+      });
+    }
+  });
 });
+
+
