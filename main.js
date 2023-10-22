47ce7d452d036b07cc77e5e5669d60a585782d26
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-22 16:17:55 +0300

    qr menu edited

--- main.js
+++ main.js
@@ -188,12 +188,14 @@ document.addEventListener("DOMContentLoaded", () => {
   }, 500);
 });
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
-  });
-}
+setTimeout(() => {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then((res) => console.log("service worker registered"))
+        .catch((err) => console.log("service worker not registered", err));
+    });
+  }
+}, 3000);
 /*** order system end ***/
--- serviceWorker.js
+++ serviceWorker.js
@@ -9,13 +9,11 @@ const assets = [
   "/images/sopung-restaurant/categories/noodles.jpg",
   "/images/sopung-restaurant/categories/sushis.jpg",
   "/images/sopung-restaurant/categories/tavuklar.jpg",
-
   "/images/sopung-restaurant/products/sebzeli-noodle.jpg",
   "/images/sopung-restaurant/products/somon-sushi.jpg",
   "/images/sopung-restaurant/products/soyali-tavuk.jpg",
   "/images/sopung-restaurant/products/tatli-eksi-noodle.jpg",
   "/images/sopung-restaurant/products/noodles.jpg",
-
   "/images/logo.png",
   "/styles/style.css"
 ];
