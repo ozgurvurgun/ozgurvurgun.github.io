b8c190596b1831d8d249e839a698d4df6b98c6ae
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-22 16:59:30 +0300

    qr menu edited

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,6 +6,16 @@ const assets = [
   "/apis/Noodles.json",
   "/apis/Sushis.json",
   "/apis/Tavuklar.json",
+  "/images/sopung-restaurant/categories/noodles.jpg",
+  "/images/sopung-restaurant/categories/sushis.jpg",
+  "/images/sopung-restaurant/categories/tavuklar.jpg",
+  "/images/sopung-restaurant/products/sebzeli-noodle.jpg",
+  "/images/sopung-restaurant/products/somon-sushi.jpg",
+  "/images/sopung-restaurant/products/soyali-tavuk.jpg",
+  "/images/sopung-restaurant/products/tatli-eksi-noodle.jpg",
+  "/images/sopung-restaurant/products/noodles.jpg",
+  "/images/logo.png",
+  "/styles/style.css"
 ];
 
 self.addEventListener("install", installEvent => {
