a7c8095b13dfb1491f35175f894dd5bff0941c8f
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-10-24 12:04:29 +0300

    edited

--- main.js
+++ main.js
@@ -185,15 +185,14 @@ function productMinus() {
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     uiDraw();
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker
+          .register("/serviceWorker.js")
+          .then((res) => console.log("service worker registered"))
+          .catch((err) => console.log("service worker not registered", err));
+      });
+    }
   }, 500);
 });
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
-  });
-}
 /*** order system end ***/
