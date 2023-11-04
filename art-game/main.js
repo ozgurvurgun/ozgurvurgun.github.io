e1ca8c3d5ef091fd9bd2038e4a7e8af24422566b
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-11-04 04:18:14 +0300

    art game edited

--- art-game/main.js
+++ art-game/main.js
@@ -10,15 +10,6 @@ let countdown = 10;
 //   }
 // }, 1000);
 
-
-
-
-
-
-
-
-
-
 async function getData() {
   const RESPONSE = await fetch("data/question.json");
   const data = await RESPONSE.json();
@@ -26,13 +17,19 @@ async function getData() {
 }
 getData();
 
-
-
-
-
-
-
-
 // document.addEventListener("contextmenu", function (e) {
 //   e.preventDefault(); // Sağ tıklama olayını engelle
 // });
+
+document.addEventListener("DOMContentLoaded", () => {
+  let pageHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight
+  );
+  console.log(pageHeight);
+  document.querySelector(".nav-right").style.height = pageHeight - 100 + "px";
+});
