8c475430ca1b71ce49e0c60d159d67f2a34cb237
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-12-13 23:22:41 +0300

    kredi faiz added and tdr machine edited

--- tdr-machine/main.js
+++ tdr-machine/main.js
@@ -16,3 +16,7 @@ upButton.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+window.onload = function() {
+  window.scrollTo(0, document.body.scrollHeight - window.innerHeight);
+};
