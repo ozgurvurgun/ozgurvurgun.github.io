248752a64bdb56548a2c0cd4e7a0552a118b4093
Ozgur VURGUN <ozgurvurgun1@gmail.com>
2023-12-19 19:57:03 +0300

    tdr machine edited

--- tdr-machine/main.js
+++ tdr-machine/main.js
@@ -1,4 +1,4 @@
-let upButton = document.querySelector(".up-wrapper");
+const upButton = document.querySelector(".up-wrapper");
 window.onscroll = () => {
   if (
     document.body.scrollTop > window.innerHeight ||
@@ -17,10 +17,24 @@ upButton.addEventListener("click", () => {
   document.documentElement.scrollTop = 0;
 });
 
-// window.onload = function() {
-//   window.scrollTo(0, document.body.scrollHeight - window.innerHeight);
-// };
+const toggleButton = document.querySelector(".toggle-btn");
+const tooggleButtonIcon = document.querySelector(".toggle-btn i");
+const dropdownMenu = document.querySelector(".dropdown-menu");
 
-// setInterval(() => {
-//   window.scrollTo(0, document.body.scrollHeight - window.innerHeight);
-// }, 50);
+toggleButton.addEventListener("click", () => {
+  const isOpen = dropdownMenu.classList.toggle("dropdown-open");
+
+  // tooggleButtonIcon.classList = isOpen
+  //   ? "fa-solid fa-xmark"
+  //   : "fa-solid fa-bars";
+  toggleButton.style.opacity = "0";
+  if (isOpen) {
+    tooggleButtonIcon.classList = "fa-solid fa-xmark";
+    toggleButton.style.opacity = "1";
+  } else {
+    tooggleButtonIcon.classList = "fa-solid fa-bars";
+    toggleButton.style.opacity = "1";
+  }
+  setTimeout(() => {
+  }, 300);
+});
