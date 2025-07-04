96d2e4a8b2ec7c943e417f93b580dc0f772e1802
Ozgur Vurgun <ozgurvurgun35@gmail.com>
2025-07-05 00:37:09 +0300

    repo has been emptied

--- README.md
+++
@@ -1,2 +0,0 @@
-# ozgurvurgun.github.io
-In this repository, I only display my projects running on the user side.
--- psy/public/js/Main.js
+++
@@ -1,7 +0,0 @@
-class Main {
-    constructor(){
-        console.log("Main constructor run.");
-    }
-}
-
-const main = new Main();
